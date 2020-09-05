cee46bfd76efb7855105a21b8d79757e606e2cdb
Dominik <48809740+deisert@users.noreply.github.com>
2020-09-05 20:25:07 +0200

    Edited base index file

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,46 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        storyblok-blog
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section class="container">
+      <nuxt-link v-for="post in posts" :key="post.id" :to="post.id">
+        <article class ="post-preview">
+          <div 
+          class="post-preview-thumbnail"
+          :style="{backgroundImage: post.thumbnailUrl}">
+
+            <div class="post-preview-content">
+              <h1>{{post.title}}</h1>
+              <p>{{post.previewText}}</p>
+            </div>
+
+          </div>
+        </article>
+      </nuxt-link>
+  </section>
 </template>
 
+
+
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      posts: [{
+          title: 'A New Beginning', 
+          previewText: 'This will be great!',
+          thumbnailUrl: 'https://toppng.com/uploads/preview/green-check-mark-11551048866etxqjezdpz.png', 
+          id: 'a-new-beginning'
+        },
+        {
+          title: 'The Second Beginning', 
+          previewText: 'This will be great, too!',
+          thumbnailUrl: 'https://toppng.com/uploads/preview/green-check-mark-11551048866etxqjezdpz.png', 
+          id: 'a-second-beginning'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
